11682e575dae981562de873d514378bf3a1a694a
Abduhamid <karalevstvabitva@gmail.com>
2025-09-03 12:27:37 +0500

    Update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const Port = Number(process.env.PORT) || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(Port, () => {
